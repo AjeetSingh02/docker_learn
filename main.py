fa75f24f34d15b529df6b67ebe94b14860bbf743
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-02-16 23:15:09 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello, How are you")
+import logging
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def intro():
+    return "HELLO. API is running."
+
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
