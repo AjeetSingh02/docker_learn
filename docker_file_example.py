f357659d5d9f60828164ce9e2297fba5aeec1507
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-02-20 17:52:55 +0530

    Update docker_file_example.py

--- docker_file_example.py
+++ docker_file_example.py
@@ -1,3 +1,5 @@
+Note: This file is named as .py file just to make it more readable.
+
 # Use the official image as a parent image
   - FROM node:current-slim
 
@@ -8,10 +10,12 @@
 
 
 # Copy the file from your host to your current location
-# COPY the file package.json from your host to the present location (.) in your image (so in this case, to /usr/src/app/package.json)
+'''COPY the file package.json from your host to the present location (.) in your image (so in this case, to /usr/src/app/package.json)'''
   - COPY package.json .
 
 # Run the command inside your image filesystem
+'''RUN the command npm install inside your image filesystem 
+   (which will read package.json to determine your app’s node dependencies, and install them)'''
   - RUN npm install
 
 # Inform Docker that the container is listening on the specified port at runtime.
